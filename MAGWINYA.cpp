7d9ef881a7fd3326befbd00803dcf7848c54157f
Palesa Ngwenya <165326438+PalesaNgwenya@users.noreply.github.com>
2026-08-18 13:33:23 +0200

    Update MAGWINYA.cpp

--- MAGWINYA.cpp
+++ MAGWINYA.cpp
@@ -1,40 +1,41 @@
 #include <iostream>
 #include<ctime>
-
 using namespace std;
 
-waitForFiveMinutes( ) {
-constant int minutes = 5; 
- constant time_t wait_time = minutes * 60 * time;
-time_t start_time = time( ) ;
-while ((time( ) - start_time ) < wait_time);
+void waitForFiveMinutes() {
+    const int minutes = 5;
+    const clock_t wait_time = minutes * 60 * CLOCKS_PER_SEC;
+
+    clock_t start_time = clock();
+    while ((clock() - start_time) < wait_time);
 }
 
-int main(){
-int temperature1, temperature2, temperature3;
+int main() {
+	int temp1, temp2, temp3;
 
- cout<<" Enter the first temperature reading: ";
- cin>> temperature1;
- waitForFiveMinutes( );
- 
- cout<< " Enter the second temperature reading:";
- cin>>temperature2;
- 
- if (temperature2 -temperature1 > 50){
-    cout<< " reduce fryer heat before taking the third reading./n";"
-    }
-    else if ( temperature2  - temperature1 < 10 )
-    cout<< " increase the fryer heat before taking the third reading./n";
-    waitForFiveMinutes( );
-    
-    cout<< "Enter the third temperature reading:"
-    cin>> temperature3;
-    
-    if ( temperature3 >= 150 &&  temperature3 <= 190 ) {
-      cout<< " You may start frying your Magwinyas./n ";
-      }
-      else {
-        cout<< " stove is not ready for frying./n" ;
-        }
-        return 0;
+	cout << "Enter the first temperature reading: ";
+	cin >> temp1;
+	waitForFiveMinutes();
+	
+	cout << "Enter the second temperature reading: ";
+        cin >> temp2;
+	
+	if (temp2 - temp1 > 50) {
+		cout << "Reduce fryer heat before taking the third reading.\n";
+	} 
+	else if (temp2 - temp1 < 10) {
+		cout << "Increase the fryer heat before taking the third reading. \n";
+	} 
+	waitForFiveMinutes();
+
+	cout << "Enter the third temperature reading: ";
+        cin >> temp3;
+
+	if (temp3 >= 150 && temp3 <= 190) {
+		cout << "You may start frying Magwinyas.\n";
+	}
+	else {
+		cout << "Oil is not ready for frying!\n";}
+	return 0;
 }
+
